932970a02a864495b448b16a8862f75bdc20fecc
shinheewon01 <shw20231@gmail.com>
2026-01-22 15:29:58 +0900

    26.01.22 hws배포를 위한준비

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,17 @@ import ordersRouter from "./routes/orders";
 import connectDB from "./db"; 
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+const allowedOrigins = process.env.CORS_ORIGIN?.split(',') || ['http://localhost:5173'];
 
-app.use(cors({origin: process.env.CORS_ORIGIN}));
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
 app.use(express.json());
 
 // Context Path 설정
-app.use("/api/v1/orders", ordersRouter);
+app.use("/v1/orders", ordersRouter);
 
 // DB 연결
 const startServer = async () => {
@@ -18,7 +22,7 @@ const startServer = async () => {
     await connectDB(); // DB 연결 대기
     app.listen(PORT, () => {
       console.log(`서버 실행중 http://localhost:${PORT}`);
-      console.log(`API: http://localhost:${PORT}/api/v1/orders`);
+      console.log(`API: http://localhost:${PORT}/v1/orders`);
     });
   } catch (err) {
     console.error("서버 시작 실패:", err);
